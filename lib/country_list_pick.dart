5324f0ccc42db208da378c417809bcea1fa7ff5c
Nabeel Ahmad <nabeel5001343@gmail.com>
2022-06-14 14:35:59 +0500

    change button to gesturedetector

--- lib/country_list_pick.dart
+++ lib/country_list_pick.dart
@@ -82,7 +82,8 @@ class _CountryListPickState extends State<CountryListPick> {
             selectedItem,
             appBar: widget.appBar ??
                 AppBar(
-                  backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+                  backgroundColor:
+                      Theme.of(context).appBarTheme.backgroundColor,
                   title: Text("Select Country"),
                 ),
             theme: theme,
@@ -100,8 +101,8 @@ class _CountryListPickState extends State<CountryListPick> {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () {
+    return GestureDetector(
+      onTap: () {
         _awaitFromSelectScreen(context, widget.appBar, widget.theme);
       },
       child: widget.pickerBuilder != null
